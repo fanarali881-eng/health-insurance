e51ae9f978a3f3fa023c3bbba9f46fbd917ea88e
fanarali881-eng <252206377+fanarali881-eng@users.noreply.github.com>
2026-02-02 16:31:22 -0500

    توسيط جميع العناصر في العمود الأخير

--- client/src/pages/SobolHome.tsx
+++ client/src/pages/SobolHome.tsx
@@ -528,9 +528,9 @@ export default function SobolHome() {
             </div>
 
             {/* Column 5 - Social Media + App Download */}
-            <div className="col-span-2">
+            <div className="col-span-2 flex flex-col items-center">
               {/* Social Media Icons */}
-              <div className="flex gap-2 md:gap-3 mb-6">
+              <div className="flex gap-2 md:gap-3 mb-6 justify-center">
                 <a href="#" className="w-8 h-8 md:w-10 md:h-10 bg-white/10 rounded-full flex items-center justify-center hover:bg-white/20">
                   <svg className="w-4 h-4 md:w-5 md:h-5" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
@@ -563,7 +563,7 @@ export default function SobolHome() {
                 </a>
               </div>
               {/* App Download */}
-              <div className="flex gap-2 md:gap-3">
+              <div className="flex gap-2 md:gap-3 justify-center">
                 <a href="#">
                   <img src="/images/app-store.png" alt="App Store" className="h-8 md:h-10" />
                 </a>
@@ -577,7 +577,7 @@ export default function SobolHome() {
                 <img src="/images/digital-stamp.png" alt="هيئة الحكومة الرقمية" className="h-12 md:h-14 brightness-0 invert" />
               </div>
               {/* Great Place To Work */}
-              <div className="mt-4 flex justify-center pr-4 md:pr-8">
+              <div className="mt-4 flex justify-center">
                 <img src="/images/great-place-to-work.png" alt="Great Place To Work" className="h-20 md:h-24" />
               </div>
             </div>

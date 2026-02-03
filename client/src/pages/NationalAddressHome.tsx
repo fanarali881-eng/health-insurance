8b98b7e369ae8f53c371f268846ef72cd35321d6
fanarali881-eng <252206377+fanarali881-eng@users.noreply.github.com>
2026-02-03 07:28:04 -0500

    تصغير ارتفاع الأزرار ورفعها ونقلها لليسار

--- client/src/pages/NationalAddressHome.tsx
+++ client/src/pages/NationalAddressHome.tsx
@@ -170,10 +170,10 @@ export default function NationalAddressHome() {
           to="/register" 
           className="absolute cursor-pointer bg-red-500/30"
           style={{
-            top: '35%',
-            right: '44%',
+            top: '28%',
+            right: '48%',
             width: '11%',
-            height: '45%'
+            height: '38%'
           }}
         >
           <span className="sr-only">العنوان الوطني للأفراد</span>
@@ -183,10 +183,10 @@ export default function NationalAddressHome() {
           to="/register" 
           className="absolute cursor-pointer bg-red-500/30"
           style={{
-            top: '35%',
-            right: '31%',
+            top: '28%',
+            right: '35%',
             width: '11%',
-            height: '45%'
+            height: '38%'
           }}
         >
           <span className="sr-only">العنوان الوطني للأعمال</span>
